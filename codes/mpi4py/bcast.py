2f7e0f5cd06967521520bd16c24eb28be82488fe
Linh Ngo <lngo@clemson.edu>
2018-02-04 22:06:45 -0500

    update

--- codes/mpi4py/bcast.py
+++ codes/mpi4py/bcast.py
@@ -8,12 +8,12 @@ elif (rank != 1):
     data = -1
 else:
     data = -2
-print ("%s: %s" % (rank, data))
+print ("Data before bcast %s: %s" % (rank, data))
 if (rank != 1):
     data = comm.bcast(data, root=0)
-    print ("%s: %s" % (rank, data))
+    print ("Data %s: %s" % (rank, data))
 else:
     data1 = comm.bcast(data, root=0)
-    print ("%s: %s" % (rank, data))
+    print ("Data %s: %s" % (rank, data))
 if (rank == 1):
-    print("%s: %s" % (rank, data1))
+    print("Data 1 %s: %s" % (rank, data1))
--- codes/mpi4py/workdist.py
+++ codes/mpi4py/workdist.py
@@ -10,4 +10,7 @@ A = [2,13,4,3,5,1,0,12,10,8,7,9,11,6,15,14]
 #print ("Elements %s and %s are assigned to process %s" 
 #       % (A[rank%15], A[1+rank%15], rank))
 if (rank < 4):
-    print ("Process %s has elements %s" % (rank, A[(4*rank):(4*rank+4)]))
+    for i in range(0,16):
+        if (i % size == rank):
+            print ("Process %s has elements %s at index %s" % \
+               (rank, A[i], i))
