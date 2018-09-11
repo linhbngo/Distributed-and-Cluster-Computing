ec8085ed40cdcdb05ef6e3a6004dac49e6a65aa6
Linh B Ngo <linh.b.ngo@gmail.com>
2018-09-11 10:45:50 -0400

    minor edits

--- codes/mpi4py/hello.py
+++ codes/mpi4py/hello.py
@@ -1,9 +1,28 @@
-#!/usr/bin/env python
-# hello.py
-from mpi4py import MPI
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
-name = MPI.Get_processor_name()
-print ("Hello world from process %s running on host %s out of %s processes" % 
-       (rank, name, size)) 
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  int size;
+  int my_rank; 
+  char proc_name[MPI_MAX_PROCESSOR_NAME];
+  int proc_name_len;
+    
+  /* Initialize the MPI environment */
+  MPI_Init(&argc, &argv);
+  
+  /* Get the number of processes */
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  /* Get the rank of the process */
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+  /* Get the name of the processor */
+  MPI_Get_processor_name(proc_name, &proc_name_len);
+
+  /* Print off a hello world message */
+  printf("Hello world from processor %s, rank %d out of %d processors\n",
+           proc_name, my_rank, size);
+
+  /* Finalize the MPI environment. */
+  MPI_Finalize();
+}
