98ab320f624e8bdba4fd47d4756cd4a781836b07
Linh B Ngo <linh.b.ngo@gmail.com>
2018-09-11 09:51:23 -0400

    update genilib

--- codes/cloudlab/xenvm.py
+++ codes/cloudlab/xenvm.py
@@ -15,12 +15,14 @@ import geni.rspec.pg as rspec
 request = portal.context.makeRequestRSpec()
 # Create a XenVM
 node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU14-64-STD"
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 node.routable_control_ip = "true"
 
 node.addService(rspec.Execute(shell="/bin/sh",
-                              command="sudo apt-get update"))
+                              command="sudo yum update"))
 node.addService(rspec.Execute(shell="/bin/sh",
-                              command="sudo apt-get install -y apache2"))
+                              command="sudo yum install -y httpd"))
+node.addService(rspec.Execute(shell="/bin/sh",
+                              command="sudo systemctl restart httpd.service"))
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
