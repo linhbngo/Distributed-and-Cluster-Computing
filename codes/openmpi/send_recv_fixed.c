87a1e4189cf7f626ea6e684ef1b2b926422e7bcd
Linh B Ngo <lngo@wcupa.edu>
2019-09-09 22:22:27 -0400

    Create send_recv_fixed.c

---
+++ codes/openmpi/send_recv_fixed.c
@@ -0,0 +1,34 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv) 
+{
+  int my_rank;       
+  int size;             
+  int tag=0;
+  int buf,i;
+  int des1,des2;
+  MPI_Status status;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  buf = my_rank; 
+
+  printf("Process %2d has original value %2d \n",my_rank,buf);
+    
+  if (my_rank == 0){
+    MPI_Send(&my_rank,1,MPI_INT,1,tag,MPI_COMM_WORLD);
+    MPI_Recv(&buf,1,MPI_INT,1,tag,MPI_COMM_WORLD,&status);
+  }
+  
+  if (my_rank == 1){
+    MPI_Recv(&buf,1,MPI_INT,0,tag,MPI_COMM_WORLD,&status);
+    MPI_Send(&my_rank,1,MPI_INT,0,tag,MPI_COMM_WORLD);  
+  }    
+  printf("Process %2d now has value %2d\n",my_rank,buf);
+
+  MPI_Finalize();
+}
