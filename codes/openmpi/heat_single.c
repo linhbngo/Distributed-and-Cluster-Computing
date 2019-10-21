14050e85f6c5c48ed8daced567db4b9a4220c5fa
Linh B Ngo <lngo@wcupa.edu>
2019-10-21 13:19:28 -0400

    Update heat_single.c

--- codes/openmpi/heat_single.c
+++ codes/openmpi/heat_single.c
@@ -1,9 +1,3 @@
-// John Utter
-// CPSC 362
-// Assignment 3
-// 9-19-2013
-// Linear implementation
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -27,98 +21,90 @@
 
 // copy new grid to the old grid
 void copyNewToOld(float grid_a[ROWS][COLS], float grid_b[ROWS][COLS]) {
-	int x, y;
-	for (x = 0; x < ROWS; ++x) {
-		for (y = 0; y < COLS; ++y) {
-			grid_b[x][y] = grid_a[x][y];
-		}
-	}
+  int x, y;
+  for (x = 0; x < ROWS; ++x) {
+    for (y = 0; y < COLS; ++y) {
+      grid_b[x][y] = grid_a[x][y];
+    }
+  }
 }
 
 // update the new grid based on the old grid
 void calculateNew(float grid_a[ROWS][COLS], float grid_b[ROWS][COLS]) {
-	int x, y;
-	for (x = 1; x < ROWS - 1; ++x) {
-		for (y = 1; y < COLS - 1; ++y) {
-			grid_a[x][y] = 0.25 * (grid_b[x-1][y] + grid_b[x+1][y] + grid_b[x][y-1] + grid_b[x][y+1]);
-		}
-	}
+  int x, y;
+  for (x = 1; x < ROWS - 1; ++x) {
+    for (y = 1; y < COLS - 1; ++y) {
+      grid_a[x][y] = 0.25 * (grid_b[x-1][y] + grid_b[x+1][y] + grid_b[x][y-1] + grid_b[x][y+1]);
+    }
+  }
 }
 
 // print the grid to a bitmat file
 void printGridtoFile(float grid[ROWS][COLS]) {
-	int x, y;
-	FILE * fp;
-	fp = fopen("c.pnm", "w");
+  int x, y;
+  FILE * fp;
+  fp = fopen("c.pnm", "w");
 
-	/* Print the P3 format header */
-    fprintf(fp, "P3\n%d %d\n15\n", COLS, ROWS);
-
-	for (x = 0; x < ROWS; ++x) {
-		for (y = 0; y < COLS; ++y) {
-			if (grid[x][y] > 250) {
-				fprintf(fp, "%s ", RED );
-			} else if (grid[x][y] > 180) {
-				fprintf(fp, "%s ", ORANGE );
-			} else if (grid[x][y] > 120) {
-				fprintf(fp, "%s ", YELLOW );
-			} else if (grid[x][y] > 80) {
-				fprintf(fp, "%s ", LTGREEN );
-			} else if (grid[x][y] > 60) {
-				fprintf(fp, "%s ", GREEN );
-			} else if (grid[x][y] > 50) {
-				fprintf(fp, "%s ", LTBLUE );
-			} else if (grid[x][y] > 40) {
-				fprintf(fp, "%s ", BLUE );
-			} else if (grid[x][y] > 30) {
-				fprintf(fp, "%s ", DARKTEAL );
-			} else if (grid[x][y] > 20) {
-				fprintf(fp, "%s ", BROWN );
-			} else {
-				// less than 20
-				fprintf(fp, "%s ", BLACK );
-			}
-		}
-		fprintf(fp, "\n");
-	}
-
-	// clean up
-    fclose(fp);
+  /* Print the P3 format header */
+  fprintf(fp, "P3\n%d %d\n15\n", COLS, ROWS);
+  for (x = 0; x < ROWS; ++x) {
+    for (y = 0; y < COLS; ++y) {
+      if (grid[x][y] > 250) {
+	fprintf(fp, "%s ", RED );
+      } else if (grid[x][y] > 180) {
+	fprintf(fp, "%s ", ORANGE );
+      } else if (grid[x][y] > 120) {
+	fprintf(fp, "%s ", YELLOW );
+      } else if (grid[x][y] > 80) {
+	fprintf(fp, "%s ", LTGREEN );
+      } else if (grid[x][y] > 60) {
+	fprintf(fp, "%s ", GREEN );
+      } else if (grid[x][y] > 50) {
+	fprintf(fp, "%s ", LTBLUE );
+      } else if (grid[x][y] > 40) {
+	fprintf(fp, "%s ", BLUE );
+      } else if (grid[x][y] > 30) {
+	fprintf(fp, "%s ", DARKTEAL );
+      } else if (grid[x][y] > 20) {
+	fprintf(fp, "%s ", BROWN );
+      } else {
+	fprintf(fp, "%s ", BLACK );
+      }
+    }
+    fprintf(fp, "\n");
+  }
+  fclose(fp);
 }
 
 int main(int argc, char **argv) {
-	int h, w, cycles, heat;
-	float grid_a[ROWS][COLS];
-	float grid_b[ROWS][COLS];
+  int h, w, cycles, heat;
+  float grid_a[ROWS][COLS];
+  float grid_b[ROWS][COLS];
 
-	if (argc != 2) {
-		printf("Usage: ./program <number of timestamps>\n");
-		exit(0);
-	}
-	cycles = atoi(argv[1]);
-	
-	// initialize two-dimensional array
-	for (h = 0; h < ROWS; ++h) {
-		for (w = 0; w < COLS; ++w) {
-			grid_a[w][h] = 20;
-		}
-	}
+  if (argc != 2) {
+    printf("Usage: ./program <number of timestamps>\n");
+    exit(0);
+  }
+  cycles = atoi(argv[1]);
 
-	// initialize a heat source
-	for (heat = 299; heat < 700; ++heat) {
-		grid_a[0][heat] = 300;
-	}
+  // initialize two-dimensional array
+  for (h = 0; h < ROWS; ++h) {
+    for (w = 0; w < COLS; ++w) {
+      grid_a[w][h] = 20;
+    }
+  }
 
-	for (cycles; cycles > 0; --cycles) {
+  // initialize a heat source
+  for (heat = 299; heat < 700; ++heat) {
+    grid_a[0][heat] = 300;
+  }
 
-		copyNewToOld(grid_a, grid_b);
+  for (cycles; cycles > 0; --cycles) {
+    copyNewToOld(grid_a, grid_b);
+    calculateNew(grid_a, grid_b);
+  }
 
-        calculateNew(grid_a, grid_b);
-	}
-
-	printGridtoFile(grid_a);
-
-	system("convert c.pnm c.png");
-
-	return 0;
+  printGridtoFile(grid_a);
+  system("convert c.pnm c.png");
+  return 0;
 }
